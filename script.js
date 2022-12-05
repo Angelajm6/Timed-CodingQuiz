41f85eff4db5fcd7da9e7d303278a5dff1631056
angelajm6 <angelajaume.m@gmail.com>
2022-12-05 15:19:18 -0500

    starter code Js

---
+++ script.js
@@ -0,0 +1,19 @@
+const startButton = document.getElementById('start-btn')
+const questionBoxElement = document.getElementById ('question-box')
+
+startButton.addEventListener('click', startGame)
+
+function startGame()) {
+console.log('Game Started')
+startButton.classList.add('hide')
+questionBoxElement.classList.remove('hide')
+setNextQuestion()
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
